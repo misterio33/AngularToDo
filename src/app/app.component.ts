6fac8d36f66d37a5aaafc341ecbcde3281f160e5
misterio33 <pasha.onoprienko@gmail.com>
2024-07-22 21:47:19 +0200

    added template for todo-item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HeaderComponent, FooterComponent],
+    imports: [HeaderComponent, FooterComponent, TodoItemComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
