346640f214b982a65d943511ef1d59c1aca8b779
misterio33 <pasha.onoprienko@gmail.com>
2024-07-22 20:12:04 +0200

    added todoItem model and base service

---
+++ src/app/models/todoItem.model.ts
@@ -0,0 +1,6 @@
+export interface TodoItem {
+    title: string;
+    description: string;
+    isDone: boolean;
+    createdAt: Date;
+}
---
+++ src/app/todo.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { TodoItem } from './models/todoItem.model';
+
+@Injectable({ providedIn: 'root' })
+export class TodoService {
+    private todoItems: TodoItem[] = [
+        {
+            title: 'title1',
+            description: 'Description for the first task',
+            isDone: false,
+            createdAt: new Date(),
+        },
+        {
+            title: 'title2',
+            description: 'Description for the second task',
+            isDone: false,
+            createdAt: new Date(),
+        },
+    ];
+
+    getTodoItems() {
+        return this.todoItems;
+    }
+}
